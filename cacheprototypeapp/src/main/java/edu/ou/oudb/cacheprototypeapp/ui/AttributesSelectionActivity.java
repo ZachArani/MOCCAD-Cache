8c9073eaff6a279dd14688b9b98b140eb1efe076
arensonjt <arensonjt@ou.edu>
2018-04-26 14:46:15 -0500

    UI fixed, it works

--- cacheprototypeapp/src/main/java/edu/ou/oudb/cacheprototypeapp/ui/AttributesSelectionActivity.java
+++ cacheprototypeapp/src/main/java/edu/ou/oudb/cacheprototypeapp/ui/AttributesSelectionActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 
@@ -23,8 +24,8 @@ import edu.ou.oudb.cacheprototypeapp.provider.ProcessedQueryDbHelper;
 public class AttributesSelectionActivity extends FragmentActivity implements View.OnClickListener {
     private ProcessedQueryDbHelper mDBHelper = null;
 
-    /*Radio Buttons*/
-    RadioButton id_button,
+    /*Toggle Buttons*/
+    ToggleButton id_button,
     patientFirst_button,
     patientLast_button,
     doctorFirst_button,
@@ -33,12 +34,9 @@ public class AttributesSelectionActivity extends FragmentActivity implements Vie
     date_time_button,
     heartrate_button;
 
-    RadioGroup attributeGroup;
-
 
     /*Buttons*/
-    private Button confirm,
-            cancel;
+    private Button confirm;
 
 
     @Override
@@ -51,7 +49,6 @@ public class AttributesSelectionActivity extends FragmentActivity implements Vie
         mDBHelper = new ProcessedQueryDbHelper(this);
 
         /*Buttons*/
-        cancel.setOnClickListener(this);
 
         confirm.setOnClickListener(this);
 
@@ -63,39 +60,26 @@ public class AttributesSelectionActivity extends FragmentActivity implements Vie
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.confirmButton:
+                System.out.println("button pressed");
                 confirmAttributes();
-                break;
-            case R.id.cancelButton:
-                cancelAttributes();
                 break;
         }
     }
 
     public void initializeVariables() {
         /*Radio Buttons*/
-        id_button = (RadioButton) findViewById(R.id.id);
-        patientFirst_button = (RadioButton) findViewById(R.id.patientfirstname);
-        patientLast_button = (RadioButton) findViewById(R.id.patientlastname);
-        doctorFirst_button = (RadioButton) findViewById(R.id.doctorfirstname);
-        doctorLast_button = (RadioButton) findViewById(R.id.doctorlastname);
-        description_button = (RadioButton) findViewById(R.id.description);
-        date_time_button = (RadioButton) findViewById(R.id.p_date_time);
-        heartrate_button = (RadioButton) findViewById(R.id.heartrate);
+        id_button = (ToggleButton) findViewById(R.id.id);
+        patientFirst_button = (ToggleButton) findViewById(R.id.patientfirstname);
+        patientLast_button = (ToggleButton) findViewById(R.id.patientlastname);
+        doctorFirst_button = (ToggleButton) findViewById(R.id.doctorfirstname);
+        doctorLast_button = (ToggleButton) findViewById(R.id.doctorlastname);
+        description_button = (ToggleButton) findViewById(R.id.description);
+        date_time_button = (ToggleButton) findViewById(R.id.p_date_time);
+        heartrate_button = (ToggleButton) findViewById(R.id.heartrate);
 
 
         /*Confirm and cancel buttons*/
-        confirm = (Button) findViewById(R.id.confirm_button);
-        cancel = (Button) findViewById(R.id.cancel_button);
-    }
-
-
-    public void onRadioButtonClicked(View view) {
-
-        switch(view.getId()){
-            case R.id.id:
-                id_button.toggle();
-                break;
-        }
+        confirm = (Button) findViewById(R.id.confirmButton);
 
     }
 
@@ -110,28 +94,23 @@ public class AttributesSelectionActivity extends FragmentActivity implements Vie
         return super.onOptionsItemSelected(item);
     }
 
-    /*When clicking on cancel, goes back to previous activity*/
-    private void cancelAttributes() {
-
-        NavUtils.navigateUpFromSameTask(AttributesSelectionActivity.this);
-    }
-
     private void confirmAttributes() {
         //"noteid", "patientfirstname", "patientlastname", "doctorfirstname", "doctorlastname", "description", "p_date_time", "heartrate"};
         ArrayList<String> attributes = new ArrayList<String>();
-        if(id_button.isSelected()) attributes.add("noteid");
-        if(patientFirst_button.isSelected()) attributes.add("patientfirstname");
-        if(patientLast_button.isSelected()) attributes.add("patientlastname");
-        if(doctorFirst_button.isSelected()) attributes.add("doctorfirstname");
-        if(doctorLast_button.isSelected()) attributes.add("doctorlastname");
-        if(description_button.isSelected()) attributes.add("description");
-        if(date_time_button.isSelected()) attributes.add("p_date_time");
-        if(heartrate_button.isSelected()) attributes.add("heartrate");
+        if(id_button.isChecked()) attributes.add("noteid");
+        if(patientFirst_button.isChecked()) attributes.add("patientfirstname");
+        if(patientLast_button.isChecked()) attributes.add("patientlastname");
+        if(doctorFirst_button.isChecked()) attributes.add("doctorfirstname");
+        if(doctorLast_button.isChecked()) attributes.add("doctorlastname");
+        if(description_button.isChecked()) attributes.add("description");
+        if(date_time_button.isChecked()) attributes.add("p_date_time");
+        if(heartrate_button.isChecked()) attributes.add("heartrate");
 
 
         Intent intent = new Intent(this, SearchExamRecordActivity.class);
         intent.putExtra(SearchExamRecordActivity.ATTRIBUTELIST, attributes);
 
         startActivity(intent);
+        System.out.println("button pressed");
     }
 }
